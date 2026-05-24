93ee33759cc50ce87b742ebe92c7d3f8ce545951
manufosela <manufosela@gmail.com>
2026-05-24 12:56:49 +0200

    fix(google): incluir drive.readonly en SCOPES de google-auth
    El scope estaba en el consent screen GCP pero NO en el array SCOPES del código. Por eso el token no incluía Drive y /drive devolvía 'insufficient authentication scopes'. Tras este fix hay que re-hacer el login para regenerar el token con los 3 scopes.

--- src/modules/google/google-auth.js
+++ src/modules/google/google-auth.js
@@ -5,6 +5,7 @@ import { google } from 'googleapis';
 const SCOPES = [
   'https://www.googleapis.com/auth/gmail.readonly',
   'https://www.googleapis.com/auth/calendar.readonly',
+  'https://www.googleapis.com/auth/drive.readonly',
 ];
 
 /**
